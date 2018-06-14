d47939b7064013ce64f1e4d9f7d0ced29f6f4d14
JoaoFernandees <joaovictors1999@gmail.com>
2018-06-14 20:54:21 -0300

    RoboCode

---
+++ FernandesUnisociesc/JoaoRobot2018.cs
@@ -0,0 +1,21 @@
+﻿using Robocode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FernandesUnisociesc
+{
+    public class JoaoRobot2018 : Robot
+    {
+        public override void Run()
+        {
+            while(true)
+            {
+                Ahead(100);
+                TurnLeft(90);
+            }
+        }
+    }
+}
